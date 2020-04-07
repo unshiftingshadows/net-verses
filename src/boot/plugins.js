3b90d0bba6defef56e81040d00c3aba9d3835783
Jacob Beck <jacob@unshiftingshadows.com>
2020-04-07 13:56:45 -0500

    Fix key exposure

--- src/boot/plugins.js
+++ src/boot/plugins.js
@@ -6,17 +6,9 @@ import 'firebase/firestore'
 import 'firebase/functions'
 import 'firebase/remote-config'
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCMi8vXfEuz3fyeowyP_-xF_RQsCO9JtmQ',
-  authDomain: 'unshiftingshadows-d31f1.firebaseapp.com',
-  databaseURL: 'https://unshiftingshadows-d31f1.firebaseio.com',
-  projectId: 'unshiftingshadows-d31f1',
-  storageBucket: 'unshiftingshadows-d31f1.appspot.com',
-  messagingSenderId: '1009170052651',
-  appId: '1:1009170052651:web:7d2f77e934d4f8b5af5c6e'
-}
+import { config } from './fbConfig'
 
-const fbapp = firebase.initializeApp(firebaseConfig)
+const fbapp = firebase.initializeApp(config)
 
 const remoteConfig = fbapp.remoteConfig()
 remoteConfig.settings = {
